c2534856a938a1d9922270ea8ea540a1294a0b96
Gaurav Pareek <me@grv.rocks>
2016-02-20 23:20:16 +0530

    Hide header from view.

--- header.php
+++ header.php
@@ -19,10 +19,9 @@
 
 				<div <?php hybrid_attr( 'branding' ); ?>>
 					<?php hybrid_site_title(); ?>
-					<?php hybrid_site_description(); ?>
 				</div><!-- #branding -->
 
-				<?php hybrid_get_menu( 'primary' ); // Loads the menu/secondary.php template. ?>
+				<?php //hybrid_get_menu( 'primary' ); // Loads the menu/secondary.php template. ?>
 
 			</div><!-- wrap -->
 		</header><!-- #header -->
