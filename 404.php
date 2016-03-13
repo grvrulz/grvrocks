47ba1be23b4146123ff54265121a08b4b9119787
Gaurav Pareek <me@grv.rocks>
2016-03-14 00:57:18 +0530

    add template for 404

---
+++ 404.php
@@ -0,0 +1,9 @@
+<?php get_header(); // Loads the header.php template. ?>
+
+<main <?php hybrid_attr( 'content' ); ?>>
+
+	<h1>404</h1>
+
+</main><!-- #content -->
+
+<?php get_footer(); // Loads the footer.php template. ?>
