7350ecbfd9c19458c16685e8663db8d1f674e34d
Gaurav Pareek <me@grv.rocks>
2016-03-14 00:58:45 +0530

    removed 404.php

--- 404.php
+++
@@ -1,9 +0,0 @@
-<?php get_header(); // Loads the header.php template. ?>
-
-<main <?php hybrid_attr( 'content' ); ?>>
-
-	<h1>404</h1>
-
-</main><!-- #content -->
-
-<?php get_footer(); // Loads the footer.php template. ?>
